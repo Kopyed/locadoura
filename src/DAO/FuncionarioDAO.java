7856e3c859cd56722efa65a79ed65dd94f3a8aac
pedro <pedro@LAPTOP-K4CNT68Q>
2019-12-02 15:37:22 -0200

    Criação do método logar()

--- src/DAO/FuncionarioDAO.java
+++ src/DAO/FuncionarioDAO.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
-/**
- *
- * @author paulo
- */
-public class FuncionarioDAO {
+import java.sql.*;
+import modelo.Funcionario;
+
+public class FuncionarioDAO extends ExecuteSQL {
+
+    public FuncionarioDAO(Connection con){
+    super(con);
+    }
+    public boolean Logar (String login, String senha){
+        boolean finalResult = false;
+    try{
+        String consulta = "select login, senha from funcionario "
+        +"where login = '"+login+"' and senha = '"+senha+"'";
+        PreparedStatement ps = getCon().prepareStatement(consulta);
+        ResultSet rs = ps.executeQuery();
+        
+         if(rs != null){
+             while (rs.next()){
+                  Funcionario a = new Funcionario();
+                  a.setLogin(rs.getString(1));
+                  a.setSenha(rs.getString(2));
+                  finalResult = true;
+             }
+             
+         } 
+        
+    }catch(SQLException ex){
+    
+        ex.getMessage();
+        
+    }return finalResult;
+   
+    
+    }
     
 }
