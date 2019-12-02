16b09118361b673a724d45456fa9d317e0cf2230
pedro <pedro@LAPTOP-K4CNT68Q>
2019-12-02 15:24:34 -0200

    Fazendo o ExecuteSQL

--- src/DAO/ExecuteSQL.java
+++ src/DAO/ExecuteSQL.java
@@ -1,5 +1,21 @@
 package DAO;
 
+import java.sql.Connection;
+
 public class ExecuteSQL {
+    
+    private Connection con;
+    
+    public ExecuteSQL(Connection con){
+        setCon(con);
+    }
+    
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
     
 }
