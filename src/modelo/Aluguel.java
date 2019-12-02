3e36a91df20be570ede95f4262a2336cd71bd806
pedro <pedro@LAPTOP-K4CNT68Q>
2019-12-02 15:20:37 -0200

    Fazendo o resto dos getters e setters

--- src/modelo/Aluguel.java
+++ src/modelo/Aluguel.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author paulo
- */
 public class Aluguel {
+    //Atributos
+    private int cod;
+    private int coddvd;
+    private int codcliente;
+    private String data_aluguel;
+    private String horario;
+    private String data_devolucao;
+    
+    //metodos getters e setters
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public int getCoddvd() {
+        return coddvd;
+    }
+
+    public void setCoddvd(int coddvd) {
+        this.coddvd = coddvd;
+    }
+
+    public int getCodcliente() {
+        return codcliente;
+    }
+
+    public void setCodcliente(int codcliente) {
+        this.codcliente = codcliente;
+    }
+
+    public String getData_aluguel() {
+        return data_aluguel;
+    }
+
+    public void setData_aluguel(String data_aluguel) {
+        this.data_aluguel = data_aluguel;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getData_devolucao() {
+        return data_devolucao;
+    }
+
+    public void setData_devolucao(String data_devolucao) {
+        this.data_devolucao = data_devolucao;
+    }
     
 }
--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,14 +1,108 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author paulo
- */
 public class Cliente {
+    
+    //atributos
+    private int Codigo;
+    private String Nome;
+    private String Nascimento;
+    private String RG;
+    private String CPF;
+    private String Telefone;
+    private String Email;
+    private String Bairro;
+    private String Rua;
+    private int Numero;
+    private String CEP;
+    
+    //metodos getters e setters
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getNascimento() {
+        return Nascimento;
+    }
+
+    public void setNascimento(String Nascimento) {
+        this.Nascimento = Nascimento;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String Bairro) {
+        this.Bairro = Bairro;
+    }
+
+    public String getRua() {
+        return Rua;
+    }
+
+    public void setRua(String Rua) {
+        this.Rua = Rua;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
     
 }
